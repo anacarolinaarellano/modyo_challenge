f34d97ed157bb4b3ba11774e31f5747b5185bd1e
Ana Carolina <anacarolinaarellano@gmail.com>
2023-04-12 20:47:13 -0600

    identifying pairs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
-import secretImg from './secret.png'
+import secretImg from "./secret.png";
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [images, setImages] = useState([]);
+  //vars to keep track of cards
+  var cardOne = "";
+  var cardTwo = "";
+  const [images, setImages] = useState([]); // use hook
   const endpoint =
     "https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=20";
 
-  // Get images from endpoint and add them ti¡o array  
+  // Get images from endpoint and add them ti¡o array
   const fetchImages = async () => {
     const response = await fetch(endpoint);
     const data = await response.json();
-    const imagesArray = data.entries;
+    var imagesArray = data.entries;
+    imagesArray = imagesArray.concat(imagesArray);
     setImages(imagesArray);
   };
 
@@ -19,11 +23,29 @@ function App() {
     fetchImages();
   }, []);
 
-  const  handleClick = (event, url) => {
-    console.log(event.target);
-    event.target.src = url
-    console.log('Image clicked');
-  }
+  const handleClick = (event, url) => {
+    cardOne = cardTwo;
+    cardTwo = event.target;
+    event.target.src = url;
+
+    if (cardOne !== "") {
+      if (cardOne.alt === cardTwo.alt) {
+        setTimeout(() => {
+          alert("Congratulations, that's a match!");
+          cardOne = "";
+          cardTwo = "";
+        }, 300);
+      } //no match
+      else {
+        setTimeout(() => {
+          cardOne.src = secretImg;
+          cardTwo.src = secretImg;
+          cardOne = "";
+          cardTwo = "";
+        }, 700);
+      }
+    }
+  };
 
   return (
     <div className="App">
@@ -32,10 +54,15 @@ function App() {
       </header>
       <div className="container">
         {images.map((currImg, idx) => {
-          const url = currImg.fields.image.url
-          const alt = currImg.fields.image.title
+          const url = currImg.fields.image.url;
+          const alt = currImg.fields.image.title;
           return (
-            <img key={idx} src={secretImg} alt={alt} onClick={event => handleClick(event, url)}/>
+            <img
+              key={idx}
+              src={secretImg}
+              alt={alt}
+              onClick={(event) => handleClick(event, url)}
+            />
           );
         })}
       </div>
