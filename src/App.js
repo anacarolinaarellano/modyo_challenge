6063ebff6f3da33dd89a726cb59afdef91ee703c
Ana Carolina <anacarolinaarellano@gmail.com>
2023-04-14 00:12:23 -0600

    responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import BootstrapSwitchButton from 'bootstrap-switch-button-react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import secretImg from "./secret.png";
 import ShowModal from "./components/Modal";
@@ -8,9 +9,11 @@ function App() {
   //vars to keep track of cards
   var cardOne = "";
   var cardTwo = "";
-  var pairsFound = 0;
-  const [images, setImages] = useState([]); // use hook
-  const [show, setShow] = useState(false);
+
+  const [images, setImages] = useState([]); // use hook for images
+  const [show, setShow] = useState(false); //hook for modal
+  const [theme, setTheme] = useState("App--light-mode"); //hook for theme
+  const [pairsFound, setPairs] = useState(0); //hook for theme
 
   const endpoint =
     "https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=20";
@@ -21,29 +24,39 @@ function App() {
     const data = await response.json();
     var imagesArray = data.entries;
     //choose first 5 random elements
-    var tempArray = randomizeArray(imagesArray)
-    tempArray = tempArray.slice(0, 5)
+    var tempArray = randomizeArray(imagesArray);
+    tempArray = tempArray.slice(0, 5);
     //duplicate the elements and randomize again
-    tempArray = tempArray.concat(tempArray)
-    console.log(tempArray)
+    tempArray = tempArray.concat(tempArray);
     setImages(randomizeArray(tempArray));
   };
 
   useEffect(() => {
     fetchImages();
-  }, []);
+    document.body.className = theme;
+  }, [theme]);
 
-  //shuffle algorithm obtained from dev.to
-  const randomizeArray = arr => {
+  //shuffle algorithm (obtained from dev.to)
+  const randomizeArray = (arr) => {
     for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       const temp = arr[i];
       arr[i] = arr[j];
       arr[j] = temp;
     }
-    return arr
-  }
+    return arr;
+  };
 
+  //switch app's theme
+  const toggleTheme = () => {
+    if (theme === "App--light-mode") {
+      setTheme("App--dark-mode");
+    } else {
+      setTheme("App--light-mode");
+    }
+  };
+
+  //logic when clicking card
   const handleClick = (event, url) => {
     cardOne = cardTwo;
     cardTwo = event.target;
@@ -51,7 +64,7 @@ function App() {
 
     if (cardOne !== "") {
       if (cardOne.alt === cardTwo.alt) {
-        pairsFound++;
+        setPairs(pairsFound + 1)
         setTimeout(() => {
           setShow(true); //show modal
           setTimeout(() => {
@@ -60,7 +73,7 @@ function App() {
           // cards that have been found now have click disabled
           cardOne.classList.add("disabled");
           cardTwo.classList.add("disabled");
-          if (pairsFound === 10)
+          if (pairsFound === 4)
             alert("You have found all the pairs! Thanks for playing!");
           //reset cards
           cardOne = "";
@@ -81,7 +94,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
       <header>
         <h1>Memory Game</h1>
       </header>
@@ -100,6 +113,10 @@ function App() {
           );
         })}
       </div>
+      <div className="switch">
+       Dark mode: <BootstrapSwitchButton onChange={toggleTheme} size="lg" onstyle="success"/>
+      </div>
+      <footer>By: Ana Carolina Arellano</footer>
     </div>
   );
 }
